bca475258d86945e7b8bec0bc7f557886e020198
Joseph Wong <wong.joseph@live.com>
2019-10-07 17:40:12 -0400

    disabled firebase functions

--- lib/Core/Services/Firebase/TransactionFirestore.dart
+++ lib/Core/Services/Firebase/TransactionFirestore.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-void firestoreSubmit(
-    String name, double amount, String category, DateTime date) {
-  Firestore.instance.collection(date.year.toString()).document().setData(
-      {'name': name, 'amount': amount, 'date': date, 'category': category});
-}
+// void firestoreSubmit(
+//     String name, double amount, String category, DateTime date) {
+//   Firestore.instance.collection(date.year.toString()).document().setData(
+//       {'name': name, 'amount': amount, 'date': date, 'category': category});
+// }
