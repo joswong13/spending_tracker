cf3e4cfcd4f1cbff77789e505266242a15fdeb96
Joseph Wong <wong.joseph@live.com>
2019-10-07 18:29:00 -0400

    made category transaction a listview

--- lib/UI/Views/CategoryTransactionView/CategoryTransactionView.dart
+++ lib/UI/Views/CategoryTransactionView/CategoryTransactionView.dart
@@ -23,40 +23,61 @@ class CategoryTransactionView extends StatelessWidget {
         child: Center(
           child: Column(
             children: <Widget>[
-              Text(
-                _categoryType,
-                style:
-                    TextStyle(fontSize: sizeConfig.topTextHeight28, color: greyCityLights, fontWeight: FontWeight.w600),
+              Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 8, 0, 0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        iconSize: sizeConfig.topTextHeight28,
+                        color: Theme.of(context).primaryColor,
+                        tooltip: "Back",
+                        onPressed: () {
+                          monthData.categoryType = "";
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          _categoryType,
+                          style: TextStyle(
+                              fontSize: sizeConfig.topTextHeight28, color: greyCityLights, fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "\$" + _amount.toString(),
+                          style: TextStyle(
+                              fontSize: sizeConfig.topTextHeight28,
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                "\$" + _amount.toString(),
-                style: TextStyle(fontSize: 28, color: greenLightGreenishBlue, fontWeight: FontWeight.w600),
-              ),
-              if (_categoryList.isEmpty)
-                Text(
-                  "No transactions for " + _categoryType,
-                  style: TextStyle(fontSize: 22, color: greyCityLights, fontWeight: FontWeight.w500),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  child: _categoryList.isEmpty
+                      ? Text(
+                          "No transactions for " + _categoryType,
+                          style: TextStyle(fontSize: 22, color: greyCityLights, fontWeight: FontWeight.w500),
+                        )
+                      : ListView.builder(
+                          itemCount: _categoryList.length,
+                          itemBuilder: (BuildContext ctx, int index) {
+                            return CategoryTransactionItem(_categoryList[index]);
+                          },
+                        ),
                 ),
-              if (_categoryList.isNotEmpty)
-                ..._categoryList.map((eachItem) {
-                  return CategoryTransactionList(eachItem);
-                }).toList(),
+              ),
             ],
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Align(
-          alignment: Alignment.bottomLeft,
-          child: FloatingActionButton.extended(
-            icon: Icon(Icons.arrow_back),
-            label: Text("Back"),
-            onPressed: () {
-              monthData.categoryType = "";
-              Navigator.pop(context);
-            },
           ),
         ),
       ),
